7fca52b0d9905664c8fec4cbc70d9074dda561bc
Weining Hu <weininghu@dhcp-128-189-86-117.ubcsecure.wireless.ubc.ca>
2018-03-30 22:42:49 -0700

    Add implementation for BipartiteMatching sampler

--- src/main/java/matchings/BipartiteMatchingSampler.java
+++ src/main/java/matchings/BipartiteMatchingSampler.java
@@ -1,5 +1,6 @@
 package matchings;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import bayonet.distributions.Multinomial;
@@ -29,7 +30,26 @@ public class BipartiteMatchingSampler implements Sampler {
 
   @Override
   public void execute(Random rand) {
-    // Fill this. 
+	  matching.getConnections();
+      List<Integer> currentConnections = matching.getConnections();
+      ArrayList<Integer> deepCurrentConnections = new ArrayList<Integer>(currentConnections);
+      double currentDensity = logDensity();
+      matching.sampleUniform(rand);
+      List<Integer> newConnections = matching.getConnections();
+      
+	  double newDensity = logDensity();
+	  double alpha = Math.min(1, Math.exp(newDensity)/Math.exp(currentDensity));
+	  double u = Math.random();
+	  if (alpha >= u) {
+		  for (int i = 0; i < newConnections.size(); i ++ ) {
+			  matching.getConnections().set(i, newConnections.get(i));
+			}
+	  } else {
+		  for (int i = 0; i < deepCurrentConnections.size(); i ++ ) {
+			  matching.getConnections().set(i, deepCurrentConnections.get(i));
+			
+		  }		  
+	  }
   }
   
   private double logDensity() {
--- src/main/java/matchings/PermutationSampler.java
+++ src/main/java/matchings/PermutationSampler.java
@@ -32,59 +32,27 @@ public class PermutationSampler implements Sampler {
 
   @Override
   public void execute(Random rand) {
-    // Fill this.
-//	  System.out.println("Previous -------------------------------------");
-permutation.getConnections();
-      
-	  
-//	  System.out.println("Current -------------------------------------");
-//	  double[] probabilities = {0.1,0.1,0.8};
-	  
-//	  long n = (long)rand.nextCategorical(probabilities) + 1;
-//	  Random curRandom = new Random(n);
-//	  permutation.sampleUniform(curRandom);
-System.out.println("New Iteration -----------------------");
-System.out.println("Current -------------------------------------");
+	  permutation.getConnections();
       List<Integer> currentConnections = permutation.getConnections();
-      System.out.println(currentConnections);
       ArrayList<Integer> deepCurrentConnections = new ArrayList<Integer>(currentConnections);
       double currentDensity = logDensity();
-      
       permutation.sampleUniform(rand);
       List<Integer> newConnections = permutation.getConnections();
       
 	  double newDensity = logDensity();
-	  System.out.println("deepCurrent -------------------------------------");
-	  System.out.println(deepCurrentConnections);
-	  System.out.println("newConnections -------------------------------------");
-	  System.out.println(newConnections);
 	  double alpha = Math.min(1, Math.exp(newDensity)/Math.exp(currentDensity));
-	  //double alpha = Math.min(1, (newDensity)/(currentDensity));
-	 //Random currand = new Random();
 	  double u = Math.random();
-	  System.out.println("Alpha is");
-	  System.out.println(alpha);
-	  System.out.println("u is");
-	  System.out.println(u);
 	  if (alpha >= u) {
-		 // permutation.setConnections(permutation.getConnection);
 		  for (int i = 0; i < newConnections.size(); i ++ ) {
 			  permutation.getConnections().set(i, newConnections.get(i));
-			
-		  }
-		  System.out.println("We decide to pick the new one");
-		  System.out.println(permutation.getConnections());
+			}
 	  } else {
 		  for (int i = 0; i < deepCurrentConnections.size(); i ++ ) {
 			  permutation.getConnections().set(i, deepCurrentConnections.get(i));
 			
-		  }
-		  System.out.println("We decide to stick with the old one");
-		  System.out.println(permutation.getConnections());
-		  
+		  }		  
 	  }
-	   	  
-  }
+}
   
   private double logDensity() {
     double sum = 0.0;
